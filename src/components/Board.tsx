608ba961fbfd402c0f2652a7b7279fc09098e28a
Yash Arora <31422627+YashArora0606@users.noreply.github.com>
2021-06-05 20:11:16 -0400

    Board css

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,28 +1,53 @@
 import React, { useEffect, useState } from 'react';
 import './Board.scss';
 
-
+enum BoardValue {
+    Empty,
+    Red,
+    Blue
+}
 
 const Board = () => {
+
+    const BOARD_SIZE = 8;
 
     const [board, setBoard] = useState<number[][]>([[]])
 
     // eslint-disable-next-line
     useEffect(() => {
-        var arr = new Array(8).fill(0).map(()=>new Array(8).fill(0));
+        var arr = new Array(BOARD_SIZE).fill(BoardValue.Empty).map(()=> {
+            return new Array(BOARD_SIZE).fill(BoardValue.Empty)
+        });
+
+        arr[4][4] = BoardValue.Red;
         setBoard(arr);
     });
 
     return (
         <div>
-            <table>
+            <table className="game-board">
                 <tbody>
                     {
                         board.map((row, i) => {
                             return (
                                 <tr>
                                 {row.map((item, j) => {
-                                    return <td>{(i + j) % 2 === 0 ? "B" : "W"}</td>;
+                                    return (
+                                        <td className={
+                                            (i + j) % 2 === 0 ? 
+                                            "dark-square" : 
+                                            "light-square"}
+                                            >
+                                        {item !== BoardValue.Empty && 
+                                        
+                                        (item === BoardValue.Red ? 
+                                            "RED" : 
+                                            "BLUE"
+                                        )
+                                        }
+                                        </td>
+                                    )
+                                        
                                 })}
                                 </tr>
                             )
